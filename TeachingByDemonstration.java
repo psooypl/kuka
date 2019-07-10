b5819d4f7d48c0b9b0d005714bfa986a87642b50
Marcin Zoltowski <psooy2011@gmail.com>
2019-07-10 12:40:31 +0200

    Usunieto nieuzywane biblioteki i zmienne

--- TeachingByDemonstration.java
+++ TeachingByDemonstration.java
@@ -1,33 +1,18 @@
 package pbl;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.util.ArrayList;
 
-import com.kuka.common.ThreadUtil;
+import java.util.ArrayList;
 import com.kuka.generated.ioAccess.MediaFlangeIOGroup;
 import com.kuka.roboticsAPI.applicationModel.RoboticsAPIApplication;
 import static com.kuka.roboticsAPI.motionModel.BasicMotions.*;
-import static com.kuka.roboticsAPI.motionModel.MMCMotions.*;
 import com.kuka.roboticsAPI.conditionModel.BooleanIOCondition;
 import com.kuka.roboticsAPI.controllerModel.Controller;
-import com.kuka.roboticsAPI.controllerModel.sunrise.mapping.HandGuidingMapper;
-import com.kuka.roboticsAPI.controllerModel.sunrise.mapping.HandGuidingModule;
 import com.kuka.roboticsAPI.deviceModel.LBR;
 import com.kuka.roboticsAPI.geometricModel.CartDOF;
 import com.kuka.roboticsAPI.geometricModel.Frame;
-import com.kuka.roboticsAPI.geometricModel.Tool;
 import com.kuka.roboticsAPI.ioModel.AbstractIO;
 import com.kuka.roboticsAPI.motionModel.HandGuidingMotion;
-import com.kuka.roboticsAPI.motionModel.RelativeLIN;
 import com.kuka.roboticsAPI.motionModel.controlModeModel.CartesianImpedanceControlMode;
-import com.kuka.roboticsAPI.motionModel.controlModeModel.HandGuidingControlMode;
-import com.kuka.roboticsAPI.uiModel.ApplicationDialogType;
 import com.kuka.roboticsAPI.uiModel.userKeys.IUserKey;
 import com.kuka.roboticsAPI.uiModel.userKeys.IUserKeyBar;
 import com.kuka.roboticsAPI.uiModel.userKeys.IUserKeyListener;
@@ -41,7 +26,7 @@ public class TeachingByDemonstration extends RoboticsAPIApplication {
 	private Controller kuka_Sunrise_Cabinet_1;
 	private AbstractIO greenButton;
 	private HandGuidingMotion handGuide;
-    private BooleanIOCondition greenButton_active;
+	private BooleanIOCondition greenButton_active;
 	private Frame nextFrame;
 	private boolean flag, movingFlag;
 	private boolean userButton;
@@ -49,26 +34,21 @@ public class TeachingByDemonstration extends RoboticsAPIApplication {
 	private int licznik = 1;
 	private int stiffness = 500;
 	private CartesianImpedanceControlMode impedanceControlMode;
-	
+
 	public void initialize() {
 
 		lbr = getContext().getDeviceFromType(LBR.class);
-		kuka_Sunrise_Cabinet_1 = getController("KUKA_Sunrise_Cabinet_1"); // inicjalizacja
-		// kontrolera
+		kuka_Sunrise_Cabinet_1 = getController("KUKA_Sunrise_Cabinet_1"); 
 		mediaFlange = new MediaFlangeIOGroup(kuka_Sunrise_Cabinet_1);
 		mediaFlange.setLEDBlue(false);
 		handGuide = new HandGuidingMotion();
-		greenButton = mediaFlange.getInput("UserButton");
-		greenButton_active = new BooleanIOCondition(greenButton, true);
-
-		frameList = new ArrayList<Frame>();
 
 		impedanceControlMode = new CartesianImpedanceControlMode();
 		impedanceControlMode.parametrize(CartDOF.X).setStiffness(stiffness);
 		impedanceControlMode.parametrize(CartDOF.Y).setStiffness(stiffness);
 		impedanceControlMode.parametrize(CartDOF.Z).setStiffness(stiffness);
 
-
+		frameList = new ArrayList<Frame>();
 
 		IUserKeyBar userBar = getApplicationUI().createUserKeyBar("User bar"); // stworzenie paska uzytkownika
 		IUserKeyListener listMoving = new IUserKeyListener() {
@@ -86,14 +66,12 @@ public class TeachingByDemonstration extends RoboticsAPIApplication {
 		userBar.publish();
 
 	}
- 
+
 	public void run() {
 
 		lbr.move(handGuide);
-		
-		while (true) {
 
-			
+		while (true) {
 			userButton = mediaFlange.getUserButton();
 
 			if (!userButton){
